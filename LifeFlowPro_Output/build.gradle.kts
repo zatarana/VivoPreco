7b3fe954208de01386638efd322595b3e41addb4
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-04-21 00:31:52 -0300

    Update build.gradle.kts

--- LifeFlowPro_Output/build.gradle.kts
+++ LifeFlowPro_Output/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.hilt.android) apply false
-    alias(libs.plugins.ksp) apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
