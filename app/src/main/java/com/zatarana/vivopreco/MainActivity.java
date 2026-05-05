48890c678383c849d42736e81006c9a755217751
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-05-04 22:06:41 -0300

    Use UX-polished WebView loader

--- app/src/main/java/com/zatarana/vivopreco/MainActivity.java
+++ app/src/main/java/com/zatarana/vivopreco/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
         settings.setAllowContentAccess(true);
         webView.setWebViewClient(new WebViewClient());
         setContentView(webView);
-        webView.loadUrl("file:///android_asset/loader-accounts.html");
+        webView.loadUrl("file:///android_asset/loader-ux.html");
     }
 
     @Override
