9225294d3808837f259a58afb839e42ad2ee321d
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-05-04 22:28:44 -0300

    Use unified design loader

--- app/src/main/java/com/zatarana/vivopreco/MainActivity.java
+++ app/src/main/java/com/zatarana/vivopreco/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
         settings.setAllowContentAccess(true);
         webView.setWebViewClient(new WebViewClient());
         setContentView(webView);
-        webView.loadUrl("file:///android_asset/loader-safe.html");
+        webView.loadUrl("file:///android_asset/loader-unified.html");
     }
 
     @Override
