b25c879a0753b8d80da9b5a2d8a906f37c78c1c9
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-05-04 22:14:10 -0300

    Use advanced debt-enabled loader

--- app/src/main/java/com/zatarana/vivopreco/MainActivity.java
+++ app/src/main/java/com/zatarana/vivopreco/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
         settings.setAllowContentAccess(true);
         webView.setWebViewClient(new WebViewClient());
         setContentView(webView);
-        webView.loadUrl("file:///android_asset/loader-mf.html");
+        webView.loadUrl("file:///android_asset/loader-debt.html");
     }
 
     @Override
