1b27c3368d92c058f47cacdaba110119687a2113
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-05-04 22:01:23 -0300

    Load finance-enhanced WebView entrypoint

--- app/src/main/java/com/zatarana/vivopreco/MainActivity.java
+++ app/src/main/java/com/zatarana/vivopreco/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
         settings.setAllowContentAccess(true);
         webView.setWebViewClient(new WebViewClient());
         setContentView(webView);
-        webView.loadUrl("file:///android_asset/index.html");
+        webView.loadUrl("file:///android_asset/loader.html");
     }
 
     @Override
