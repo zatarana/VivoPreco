7e5b9e7f36a00476e9b1ee979dc9234f01a968c6
Bitolinha <117381124+zatarana@users.noreply.github.com>
2026-05-04 23:57:31 -0300

    Clean Core: register planning actions

--- app/src/main/assets/clean/js/app-extensions.js
+++ app/src/main/assets/clean/js/app-extensions.js
@@ -30,6 +30,25 @@ window.AppExtensions=(function(){
     if(action==='editWallet'){FinanceUI.editWalletForm(id);return true;}
     if(action&&action.startsWith('saveEditWallet:')){FinanceUI.saveEditWallet(action.split(':')[1]);return true;}
     if(action==='deleteWallet'){FinanceUI.deleteWallet(id);return true;}
+
+    if(action==='showPlanning'){PlanningUI.showHub(data());return true;}
+    if(action==='showCategories'){PlanningUI.showCategories(data());return true;}
+    if(action==='newCategory'){PlanningUI.newCategoryForm();return true;}
+    if(action==='saveCategory'){PlanningUI.saveCategory();return true;}
+    if(action==='deleteCategory'){PlanningUI.deleteCategory(id);return true;}
+    if(action==='showBudgets'){PlanningUI.showBudgets(data());return true;}
+    if(action==='newBudget'){PlanningUI.newBudgetForm();return true;}
+    if(action==='saveBudget'){PlanningUI.saveBudget();return true;}
+    if(action==='showGoals'){PlanningUI.showGoals(data());return true;}
+    if(action==='newGoal'){PlanningUI.newGoalForm();return true;}
+    if(action==='saveGoal'){PlanningUI.saveGoal();return true;}
+    if(action==='contributeGoal'){PlanningUI.contributeGoalForm(id);return true;}
+    if(action&&action.startsWith('saveGoalContribution:')){PlanningUI.saveGoalContribution(action.split(':')[1]);return true;}
+    if(action==='deleteGoal'){PlanningUI.deleteGoal(id);return true;}
+    if(action==='showCards'){PlanningUI.showCards(data());return true;}
+    if(action==='newCard'){PlanningUI.newCardForm();return true;}
+    if(action==='saveCard'){PlanningUI.saveCard();return true;}
+    if(action==='deleteCard'){PlanningUI.deleteCard(id);return true;}
     return false;
   }
   function install(){
